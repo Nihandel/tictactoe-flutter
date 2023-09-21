9b1e082cb76d50590ce38cf7c292800341392d65
francesco finotto <finottofrancesco1@gmail.com>
2023-09-21 16:22:04 +0200

    basic game logic

--- tictactoeflutter/lib/home_page/home_page.dart
+++ tictactoeflutter/lib/home_page/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:tictactoeflutter/ttt_button/ttt_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late List<List<TTTButtonState>> states;
+  late TTTButtonState turn;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    turn = TTTButtonState.X;
+    states = List.generate(
+        3,
+        (index) => List<TTTButtonState>.generate(
+              3,
+              (index) => TTTButtonState.clear,
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var mediaquery = MediaQuery.of(context).size;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(child: Text("header")),
+        Expanded(
+          child: Table(
+            border: TableBorder.all(color: Colors.red, width: 1),
+            children: states
+                .mapIndexed(
+                  (rowIndex, row) => TableRow(
+                    children: row
+                        .mapIndexed(
+                          (index, state) => TTTButton(
+                            onClick: () => onButtonClick(rowIndex, index),
+                            state: state,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ],
+    );
+  }
+  void onButtonClick(int row,int col) => setState(() {
+    states[row][col]=turn;
+    turn = turn == TTTButtonState.X? TTTButtonState.O:TTTButtonState.X;
+    if(checkWinner()){
+      showDialog(context: context, builder:(context) => AlertDialog(content: Text("Winner"),),);
+    }
+  });
+  bool checkWinner(){
+    List<List<int>> victoryConditions=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+    for(int i=0;i<victoryConditions.length;i++){
+      var victory = victoryConditions[i];
+      TTTButtonState a = states[victory[0]~/3][(victory[0]%3)];
+      TTTButtonState b = states[victory[1]~/3][(victory[1]%3)];
+      TTTButtonState c = states[victory[2]~/3][(victory[2]%3)];
+      if(a!=TTTButtonState.clear && a==b && b==c) 
+      {
+        return true;
+      }
+      
+    }
+    return false;
   }
 }
---
+++ tictactoeflutter/lib/ttt_button/ttt_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+enum TTTButtonState{
+  X,O,clear;
+}
+
+class TTTButton extends StatelessWidget {
+  const TTTButton({required this.onClick,this.state = TTTButtonState.clear, super.key});
+  final VoidCallback onClick;
+  final TTTButtonState state;
+  @override
+  Widget build(BuildContext context) {
+    late final Widget child;
+    switch(state){
+      case TTTButtonState.X:
+        child = const Icon(Icons.close);
+      break;
+      case TTTButtonState.O:
+        child = const Icon(Icons.radio_button_unchecked);
+      break;
+      default:
+        child = Container();
+    }
+    return IconButton(onPressed: state == TTTButtonState.clear? onClick: null, icon: child);
+  }
+}
